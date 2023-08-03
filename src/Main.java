b9c4b8e8ad64f5a43e8c2085a6d9efa0cb8620a5
doriancrutcher <doriankinoocrutcher@gmail.com>
2023-08-03 11:03:26 -0700

    Deleted Dog test file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
       Stopwatch stopwatch = new Stopwatch();
-//       everything else is done in the stopwatch class
-//        Dog Nala= new Dog("Nala", 32);
-//        Nala.myDog();
+
     }
 }
